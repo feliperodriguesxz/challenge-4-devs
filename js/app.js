655fc865cf1c2b7ca70ab7c6c349ee81c77daea7
feliperodriguesxz <feliperodriguesxz@gmail.com>
2018-10-08 19:57:29 -0300

    POST on firebase

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyCRIlX6zSoHDlrJdozA_eI1dCuxoTfWNN8",
+    authDomain: "challenge-4-devs.firebaseapp.com",
+    databaseURL: "https://challenge-4-devs.firebaseio.com",
+    projectId: "challenge-4-devs",
+    storageBucket: "challenge-4-devs.appspot.com",
+    messagingSenderId: "764592048564"
+};
+firebase.initializeApp(config);
+
+  // Initialize Firebase
+  //http-server -c1 - cache 1 seconds
+  //bower init
+  //npm init
---
+++ js/insert_customer.js
@@ -0,0 +1,28 @@
+var clientName = document.getElementById('clientName');
+var clientContact = document.getElementById('clientContact');
+var clientDate = document.getElementById('clientDate');
+var save = document.getElementById('save');
+
+save.addEventListener('click', function () {
+var clientContact = document.getElementById('clientContact');
+    create(clientName.value, clientContact.value, clientDate.value);
+});
+
+function create(clientName, clientContact, clientDate) {
+    var data = {
+        clientName: clientName,
+        clientContact: clientContact,
+        clientDate: clientDate
+    };
+
+    return firebase.database().ref().child('customers').push(data);
+}
+
+firebase.database().ref('customers').on('value', function (snapshot) {
+    customersList.innerHTML = '';
+    snapshot.forEach(function (item) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().clientName + ': ' + item.val().clientContact + ': '+ item.val.clientDate));
+        customersList.appendChild(li);
+    });
+});
