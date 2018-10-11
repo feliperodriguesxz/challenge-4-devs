668565d4c5d5170a583add911c0e0e3fbe74701c
feliperodriguesxz <feliperodriguesxz@gmail.com>
2018-10-10 21:48:16 -0300

    changes in insert_customer.js

--- js/insert_customer.js
+++ js/insert_customer.js
@@ -5,7 +5,7 @@ var clientDate = document.getElementById('clientDate');
 var save = document.getElementById('save');
 
 save.addEventListener('click', function () {
-var clientContact = document.getElementById('clientContact');
+    var clientContact = document.getElementById('clientContact');
     create(clientName.value, clientContact.value, clientDate.value);
 });
 
@@ -23,7 +23,7 @@ firebase.database().ref('customers').on('value', function (snapshot) {
     customersList.innerHTML = '';
     snapshot.forEach(function (item) {
         var li = document.createElement('li');
-        li.appendChild(document.createTextNode(item.val().clientName + ': ' + item.val().clientContact + ': '+ item.val().clientDate));
+        li.appendChild(document.createTextNode(item.val().clientName + ': ' + item.val().clientContact + ': ' + item.val().clientDate));
         customersList.appendChild(li);
     });
 });
